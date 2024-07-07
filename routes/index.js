fa8a116034979cd258f4d540c3a175c56314dc8a
Boban Kilvin <btrajcevski10@gmail.com>
2024-07-06 21:11:37 -0400

    add item and category routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const item_controller = require("../controllers/item_controller");
+const category_controller = require("../controllers/category_controller");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.redirect("/items");
 });
+
+router.get("/items", item_controller.item_list);
+router.get("/item/:id", item_controller.item_detail);
+router.get("/item/create", item_controller.item_create_get);
+router.post("/item/create", item_controller.item_create_post);
+router.get("/item/:id/update", item_controller.item_update_get);
+router.post("/item/:id/update", item_controller.item_update_post);
+router.get("/item/:id/delete", item_controller.item_delete_get);
+router.post("/item/:id/delete", item_controller.item_delete_post);
+
+router.get("/categories", category_controller.categories_list);
+router.get("/category/:id", category_controller.category_detail);
+router.get("/category/create", category_controller.category_create_get);
+router.post("/category/create", category_controller.category_create_post);
+router.get("/category/:id/update", category_controller.category_update_get);
+router.post("/category/:id/update", category_controller.category_update_post);
+router.get("/category/:id/delete", category_controller.category_delete_get);
+router.post("/category/:id/delete", category_controller.category_delete_post);
 
 module.exports = router;
