c346d0d9e03a7e2af50340129a4f552e5a5f6714
Boban Kilvin <btrajcevski10@gmail.com>
2024-07-10 22:50:01 -0400

    Change router order to appropriately route to create

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,18 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/items", item_controller.item_list);
-router.get("/item/:id", item_controller.item_detail);
 router.get("/item/create", item_controller.item_create_get);
 router.post("/item/create", item_controller.item_create_post);
+router.get("/item/:id", item_controller.item_detail);
 router.get("/item/:id/update", item_controller.item_update_get);
 router.post("/item/:id/update", item_controller.item_update_post);
 router.get("/item/:id/delete", item_controller.item_delete_get);
 router.post("/item/:id/delete", item_controller.item_delete_post);
 
 router.get("/categories", category_controller.categories_list);
-router.get("/category/:id", category_controller.category_detail);
 router.get("/category/create", category_controller.category_create_get);
 router.post("/category/create", category_controller.category_create_post);
+router.get("/category/:id", category_controller.category_detail);
 router.get("/category/:id/update", category_controller.category_update_get);
 router.post("/category/:id/update", category_controller.category_update_post);
 router.get("/category/:id/delete", category_controller.category_delete_get);
