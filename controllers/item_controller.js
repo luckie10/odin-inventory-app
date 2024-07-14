e61d35f1e10da8b6924f9b4e3d169db348571db6
Boban Kilvin <btrajcevski10@gmail.com>
2024-07-13 21:57:00 -0400

    Add item update form

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -72,20 +72,76 @@ exports.item_create_post = [
       });
     }
 
-    console.log(req.body);
-
     await item.save();
     res.redirect(item.url);
   }),
 ];
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update GET");
+  const [item, allCategories] = await Promise.all([
+    Item.findById(req.params.id).populate("category").exec(),
+    Category.find().sort({ name: 1 }).exec(),
+  ]);
+
+  if (!item) {
+    const err = new Error("Item does not exist");
+    err.status = 404;
+    next(err);
+  }
+
+  res.render("item_form", {
+    title: `Update item`,
+    item: item,
+    all_categories: allCategories,
+  });
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update POST");
-});
+exports.item_update_post = [
+  body("name", "Name must be at least 2 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("category", "Must select a category")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description").trim().escape(),
+  body("price", "Must specify an item price.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("stock", "Must specify a stock amount.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+      return res.render("item_form", {
+        title: "Create Item",
+        all_categories: allCategories,
+        item: item,
+        errors: errors.array(),
+      });
+    }
+
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+    res.redirect(updatedItem.url);
+  }),
+];
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Item delete GET");
